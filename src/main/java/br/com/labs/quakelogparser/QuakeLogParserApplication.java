78d34285e064ed83ad6427a5525c9c6feb3c5356
Marco Souza <t1102mrc@prestadorcbmp.com.br>
2019-04-20 15:02:16 -0300

    Including the project structure using spring boot

---
+++ src/main/java/br/com/labs/quakelogparser/QuakeLogParserApplication.java
@@ -0,0 +1,13 @@
+package br.com.labs.quakelogparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuakeLogParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuakeLogParserApplication.class, args);
+	}
+
+}
