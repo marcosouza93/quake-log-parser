3bfa37060ddadbe920afff2cafc1cac9fe007fcd
Marco Souza <t1102mrc@prestadorcbmp.com.br>
2019-04-28 22:32:09 -0300

    Removing unused import

--- src/main/java/br/com/labs/quakelogparser/usecase/GameReportGenerator.java
+++ src/main/java/br/com/labs/quakelogparser/usecase/GameReportGenerator.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
